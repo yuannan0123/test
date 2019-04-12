e28e497a7d2a26d15b93f80b0895d94d6fc1f80b
yuannan0123 <554606686@qq.com>
2019-04-12 11:20:53 +0800

    首页完成

--- js/brand.js
+++ js/brand.js
@@ -1,24 +1,30 @@
 $(function () {
-  $.ajax({
-    type:'get',
-    url:'getbrand',
-    data: { "brandtitleid":Number},
-    dataType:'json',
-    success:function(result) {
-      console.log(result)
-      var html = template('productTemp',result)
-      $('.productList').html(html)
-    }
-  })
+  
+  
+  getbrandList()
+  function getbrandList(brandtitleid) {
+    $.ajax({
+      type: 'get',
+      url: 'getbrand',
+      data: { "brandtitleid": Number },
+      dataType: 'json',
+      success: function (result) {
+        console.log(result)
+        var html = template('productTemp', result)
+        $('.productList').html(html)
+      }
+    })
+  }
+  
 
   // 品牌标题对应的十大品牌的销量排行商品列表
-  $.ajax({
-    type:'get',
-    url:'getbrandproductlist',
-    data: { brandtitleid:Number},
-    dataType:'json',
-    success:function(result) {
-      console.log(result)
-    }
-  })
+  // $.ajax({
+  //   type:'get',
+  //   url:'getbrandproductlist',
+  //   data: { brandtitleid:Number},
+  //   dataType:'json',
+  //   success:function(result) {
+  //     console.log(result)
+  //   }
+  // })
 })

9a055bfc9d6241a6b4e700701296ce473604b6c0
yuannan0123 <554606686@qq.com>
2019-04-12 14:39:52 +0800

    首页完成1111111111111

--- js/brand.js
+++ js/brand.js
@@ -1,17 +1,26 @@
 $(function () {
   
-  
+  console.log(location.search) // 当前地址栏问号后面的数据
+  console.log($.querystring(location.search))  //{brandTitleId: "2"}
+  var aa = $.querystring(location.search)     //{brandTitleId: "2"}
+  var bb = aa.brandTitleId     // 2
+  console.log(bb)
   getbrandList()
-  function getbrandList(brandtitleid) {
+  function getbrandList() {
     $.ajax({
       type: 'get',
       url: 'getbrand',
-      data: { "brandtitleid": Number },
+      data: { brandtitleid:bb},
       dataType: 'json',
       success: function (result) {
         console.log(result)
         var html = template('productTemp', result)
         $('.productList').html(html)
+
+        // 修改动态生成结构的前三个span的背景色
+        $('.product:nth-of-type(1) span').css("background-color", 'red')
+        $('.product:nth-of-type(2) span').css("background-color", '#ff9314')
+        $('.product:nth-of-type(3) span').css("background-color", '#8adf5b')
       }
     })
   }
